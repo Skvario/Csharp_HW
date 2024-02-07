874d5551fe66e97f06affc635b413e2ba8c87fc0
Dmitry <skvar1989@gmail.com>
2024-02-08 02:44:25 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿int Ackerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+    {
+        if (n == 0 && m > 0)
+        {
+            return Ackerman(m - 1, 1);
+        }
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+    }
+}
+Console.Write("Введите число m: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Ackerman(m, n));
