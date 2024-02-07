53d3407c4d453a5846ea98c7bc029078b2ba7fb3
Dmitry <skvar1989@gmail.com>
2024-02-07 23:57:40 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int m = 5;
+        int n = 10;
+
+        PrintNumbers(m, n);
+    }
+
+    static void PrintNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNumbers(m + 1, n);
+        }
+    }
+}
