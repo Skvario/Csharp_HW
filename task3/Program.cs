9509e5be9eda7076f65d4ce247729d25e0f45329
Dmitry <skvar1989@gmail.com>
2024-02-08 03:00:05 +0300

    Task3

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr = { 1, 2, 3, 4, 5 };
+
+    
+ PrintArray(arr, arr.Length - 1);
+
+
+ void PrintArray(int[] arr, int index)
+{
+    
+    if (index < 0) return;
+
+    Console.WriteLine(arr[index]);
+    PrintArray(arr, index - 1);
+}
